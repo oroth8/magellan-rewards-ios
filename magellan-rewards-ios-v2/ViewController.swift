b0782175bb06b680d24f162f15a20bd8b1695764
Owen Roth <69156111+oroth8@users.noreply.github.com>
2024-02-12 22:45:48 -0600

    Initial Commit

---
+++ magellan-rewards-ios-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magellan-rewards-ios-v2
+//
+//  Created by Owen Roth on 2/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
